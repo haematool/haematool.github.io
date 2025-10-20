04f277a849bbd6a0daa80ca8c0b345fe29ab240b
haematool <emmanuelsleiman@gmail.com>
2025-10-20 17:42:23 +0000

    Deploying to gh-pages from @ haematool/haematool.github.io@458790d9763d4045427c5602e6f5ee6d6b7b8980 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please find below my curriculum vitae details",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
